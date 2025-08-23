d79cac8ec91721485d1d70eadef39a6c7304d106
Sarah Vasquez Gonzalez <SVASGON@upv.edu.es>
2025-08-23 19:11:19 +0200

    Remaining pages to React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,15 +1,20 @@
 import {Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 import './App.css'
 
 function App() {
 
-//path = "/" es igual a index
+//path = "/" es i gual a index
   return (
     <Routes>
       <Route index element= {<HomePage/>} /> 
       <Route path="checkout" element= {<CheckoutPage/>} />   
+      <Route path="orders" element= {<OrdersPage/>} />   
+      <Route path="tracking" element= {<TrackingPage/>} />   
+
     </Routes>
      
   )
